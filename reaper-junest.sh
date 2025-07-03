97fdff04a015305a4515b7dc1148ee7cb9cee3f0
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-07-03 22:14:14 +0200

    Update reaper-junest.sh - add libpulse pipewire-jack pulseaudio reapack

--- reaper-junest.sh
+++ reaper-junest.sh
@@ -2,7 +2,7 @@
 
 APP=reaper
 BIN="$APP" #CHANGE THIS IF THE NAME OF THE BINARY IS DIFFERENT FROM "$APP" (for example, the binary of "obs-studio" is "obs")
-DEPENDENCES="" #SYNTAX: "APP1 APP2 APP3 APP4...", LEAVE BLANK IF NO OTHER DEPENDENCIES ARE NEEDED
+DEPENDENCES="libpulse pipewire-jack pulseaudio reapack" #SYNTAX: "APP1 APP2 APP3 APP4...", LEAVE BLANK IF NO OTHER DEPENDENCIES ARE NEEDED
 #BASICSTUFF="binutils debugedit gzip"
 #COMPILERS="base-devel"
 
